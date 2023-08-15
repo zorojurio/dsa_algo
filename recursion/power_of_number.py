f8c6aaf7996ac6c2ad77464a897a836ba297dd7a
luffy <zorojurio@gmail.com>
2023-08-15 08:35:06 +0530

    handle negative numbers

--- recursion/power_of_number.py
+++ recursion/power_of_number.py
@@ -1,13 +1,15 @@
 
 
 def cal_power_of_number(number, power):
-    if power < 1:
+    if power == 0:
         return 1
+    elif power < 0:
+        return 1 / number * cal_power_of_number(number, power+1)
     return cal_power_of_number(number, power-1) * number
 
 
 if __name__ == '__main__':
-    print(cal_power_of_number(2, 2))
+    print(cal_power_of_number(2, -2))
     print(cal_power_of_number(2, 4))
     print(cal_power_of_number(4, 4))
     print(cal_power_of_number(50, 50))
