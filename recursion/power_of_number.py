f313b17cd589ab70b9a5f4d80574307eec0a6e28
luffy <zorojurio@gmail.com>
2023-08-15 08:23:14 +0530

    power of number using recursion

---
+++ recursion/power_of_number.py
@@ -0,0 +1,14 @@
+
+
+def cal_power_of_number(number, power):
+    if power < 1:
+        return 1
+    return cal_power_of_number(number, power-1) * number
+
+
+if __name__ == '__main__':
+    print(cal_power_of_number(2, 2))
+    print(cal_power_of_number(2, 4))
+    print(cal_power_of_number(4, 4))
+    print(cal_power_of_number(50, 50))
+    print(cal_power_of_number(1, 50))
