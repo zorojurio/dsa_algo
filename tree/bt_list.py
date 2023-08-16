5f2cc6aaa56e60596ac9f4692182cb96e0136965
luffy <zorojurio@gmail.com>
2023-08-16 14:23:25 +0530

    insert a node for a binary tree created using lists

--- tree/bt_list.py
+++ tree/bt_list.py
@@ -1,11 +1,24 @@
 
-
 class BinaryTree:
     def __init__(self, size):
         self.custom_list = [None] * size
         self.last_used_index = 0
         self.max_size = size
 
+    def insert(self, value):
+        if self.last_used_index + 1 == self.max_size:
+            return "Full"
+        else:
+            self.custom_list[self.last_used_index+1] = value
+            self.last_used_index += 1
 
 
 new_binary_tree = BinaryTree(8)
+
+new_binary_tree.insert('Drinks')
+new_binary_tree.insert('Hot')
+new_binary_tree.insert('Cold')
+new_binary_tree.insert('Tea')
+new_binary_tree.insert('Coffee')
+print(new_binary_tree.custom_list)
+
