0cf218734f338ffe4972985d97aaa67c777f81eb
luffy <zorojurio@gmail.com>
2023-08-16 14:08:19 +0530

    binary tree using python lists

---
+++ tree/bt_list.py
@@ -0,0 +1,11 @@
+
+
+class BinaryTree:
+    def __init__(self, size):
+        self.custom_list = [None] * size
+        self.last_used_index = 0
+        self.max_size = size
+
+
+
+new_binary_tree = BinaryTree(8)
