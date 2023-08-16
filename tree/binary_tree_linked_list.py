47da28b64557a5163b939cbcc913bdc094508ceb
luffy <zorojurio@gmail.com>
2023-08-16 09:10:23 +0530

    preorder traversal

---
+++ tree/binary_tree_linked_list.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+    def __str__(self):
+        return f"{self.data}"
+
+
+def preorder_traversal(root_node):
+    if root_node is None:
+        return None
+    print(root_node.data)
+    preorder_traversal(root_node.left_child)
+    preorder_traversal(root_node.right_child)
+
+
+if __name__ == '__main__':
+    tree = TreeNode('Drinks')
+    left_child = TreeNode('Hot')
+    right_child = TreeNode('Cold')
+    tree.left_child = left_child
+    tree.right_child = right_child
+    tea = TreeNode('Tea')
+    coffee = TreeNode('Coffee')
+    left_child.left_child = tea
+    left_child.right_child = coffee
+
+    preorder_traversal(tree)
