2659892f79b9db78506ca42fa4e4e5e99d420164
luffy <zorojurio@gmail.com>
2023-08-24 16:22:13 +0530

    self balance when inserting a node into avl tree

--- binary_search_tree/avl.py
+++ binary_search_tree/avl.py
@@ -8,6 +8,9 @@ class AVlNode:
         self.left_child = None
         self.height = 1
 
+    def __repr__(self):
+        return f"{self.data}"
+
 
 def level_order_traversal(root_node: AVlNode):
     if root_node is None:
@@ -29,7 +32,7 @@ def pre_order_traversal(root_node: AVlNode):
         return None
     print(root_node.data, end='->')
     pre_order_traversal(root_node.left_child)
-    pre_order_traversal(root_node.left_child)
+    pre_order_traversal(root_node.right_child)
 
 
 def in_order_traversal(root_node: AVlNode):
@@ -38,7 +41,7 @@ def in_order_traversal(root_node: AVlNode):
     else:
         in_order_traversal(root_node.left_child)
         print(root_node.data, end='->')
-        in_order_traversal(root_node.left_child)
+        in_order_traversal(root_node.right_child)
 
 
 def post_order_traversal(root_node: AVlNode):
@@ -46,7 +49,7 @@ def post_order_traversal(root_node: AVlNode):
         return None
     else:
         post_order_traversal(root_node.left_child)
-        post_order_traversal(root_node.left_child)
+        post_order_traversal(root_node.right_child)
         print(root_node.data, end='->')
 
 
@@ -67,5 +70,83 @@ def search(root_node: AVlNode, value):
             search(root_node.right_child, value)
 
 
+def get_height(root_node):
+    if not root_node:
+        return 0
+    return root_node.height
+
+
+def rotate_right(imbalanced_node: AVlNode) -> AVlNode:
+    new_root: AVlNode = imbalanced_node.left_child
+    imbalanced_node.left_child = imbalanced_node.left_child.right_child
+    new_root.right_child = imbalanced_node
+    imbalanced_node.height = 1 + max(get_height(imbalanced_node.right_child), get_height(imbalanced_node.left_child))
+    new_root.height = 1 + max(get_height(new_root.right_child), get_height(new_root.left_child))
+    return new_root
+
+
+def rotate_left(imbalanced_node: AVlNode) -> AVlNode:
+    new_root: AVlNode = imbalanced_node.right_child
+    imbalanced_node.right_child = imbalanced_node.right_child.left_child
+    new_root.left_child = imbalanced_node
+    imbalanced_node.height = 1 + max(get_height(imbalanced_node.right_child), get_height(imbalanced_node.left_child))
+    new_root.height = 1 + max(get_height(new_root.right_child), get_height(new_root.left_child))
+    return new_root
+
+
+def get_balance(root_node: AVlNode):
+    if not root_node:
+        return 0
+    return get_height(root_node.left_child) - get_height(root_node.right_child)
+
+
+def insert_node(root_node: AVlNode, node_value):
+    if root_node is None:
+        return AVlNode(node_value)
+    elif node_value < root_node.data:
+        node = insert_node(root_node.left_child, node_value)
+        root_node.left_child = node
+    else:
+        node = insert_node(root_node.right_child, node_value)
+        root_node.right_child = node
+    root_node.height = 1 + max(get_height(root_node.left_child), get_height(root_node.right_child))
+    balance = get_balance(root_node)
+    if balance > 1 and node_value < root_node.left_child.data:
+        rotate_node = rotate_right(root_node)
+        return rotate_node
+    if balance > 1 and node_value > root_node.left_child.data:
+        rotate_left_node = rotate_left(root_node.left_child)
+        root_node.left_child = rotate_left_node
+        rotate_node = rotate_right(root_node)
+        return rotate_node
+    if balance < -1 and node_value > root_node.right_child.data:
+        rotate_node = rotate_left(root_node)
+        return rotate_node
+    if balance < -1 and node_value < root_node.right_child.data:
+        rotate_right_node = rotate_right(root_node.right_child)
+        root_node.right_child = rotate_right_node
+        rotate_node = rotate_left(root_node)
+        return rotate_node
+    return root_node
+
+
+def delete_node(root_node: AVlNode, node_value):
+    if root_node is None:
+        return root_node
+    elif node_value < root_node.data:
+        root_node.left_child = delete_node(root_node.left_child, node_value)
+    elif node_value > root_node.data:
+        root_node.right_child = delete_node(root_node.right_child, node_value)
+
+
 if __name__ == '__main__':
-    avl_tree = AVlNode(10)
+    avl_tree = AVlNode(5)
+    avl_tree = insert_node(avl_tree, 10)
+    avl_tree = insert_node(avl_tree, 15)
+    level_order_traversal(avl_tree)
+    print()
+    avl_tree = insert_node(avl_tree, 20)
+    level_order_traversal(avl_tree)
+    print()
+    avl_tree = insert_node(avl_tree, 25 )
+    level_order_traversal(avl_tree)
