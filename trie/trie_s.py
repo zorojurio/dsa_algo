097e18e3cee5f0b7548778352f45a6bf54e7284b
luffy <zorojurio@gmail.com>
2023-08-26 10:39:03 +0530

    search trie

--- trie/trie_s.py
+++ trie/trie_s.py
@@ -21,9 +21,24 @@ class Trie:
         current.end_of_string = True
         print('Added to Trie')
 
+    def search_string(self, word: str) -> bool:
+        current_node: TrieNode = self.root_node
+        for i in word:
+            node = current_node.children.get(i)
+            if node is None:
+                return False
+            current_node = node
+        if current_node.end_of_string:
+            return True
+        return False
+
 
 if __name__ == '__main__':
     new_trie = Trie()
     new_trie.insert_string('APP')
     new_trie.insert_string('APL')
     new_trie.insert_string('API')
+    new_trie.insert_string('APIS')
+    print(new_trie.search_string('AP'))
+    print(new_trie.search_string('API'))
+    print(new_trie.search_string('APIS'))
