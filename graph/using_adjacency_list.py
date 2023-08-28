95d038150e68f1b05bde615697db001ae6bf539f
luffy <zorojurio@gmail.com>
2023-08-28 20:36:46 +0530

    breadth first search graph

--- graph/using_adjacency_list.py
+++ graph/using_adjacency_list.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 
 class Graph:
@@ -31,6 +32,26 @@ class Graph:
             print(e)
             return False
 
+    def remove_vertex(self, vertex):
+        if vertex in self.adjacency_list:
+            for other_vertex in self.adjacency_list[vertex]:
+                self.adjacency_list[other_vertex].remove(vertex)
+            del self.adjacency_list[vertex]
+            return True
+        return False
+
+    def breath_first_search(self, vertex: str) -> None:
+        visited = set()
+        visited.add(vertex)
+        queue = deque(vertex)
+        while queue:
+            current_vertex = queue.popleft()
+            print(current_vertex)
+            for adjacency_vertex in self.adjacency_list[current_vertex]:
+                if adjacency_vertex not in visited:
+                    visited.add(adjacency_vertex)
+                    queue.append(adjacency_vertex)
+
 
 if __name__ == '__main__':
     custom_graph = Graph()
@@ -38,11 +59,26 @@ if __name__ == '__main__':
     custom_graph.add_vertex('b')
     custom_graph.add_vertex('c')
     custom_graph.add_vertex('d')
+    custom_graph.add_vertex('e')
     custom_graph.add_edge('a', 'b')
-    custom_graph.add_edge('b', 'c')
+    custom_graph.add_edge('b', 'e')
+    custom_graph.add_edge('e', 'd')
+    custom_graph.add_edge('d', 'c')
     custom_graph.add_edge('c', 'a')
+    # print(custom_graph)
+    # custom_graph.remove_edge('a', 'd')
+    # custom_graph.remove_edge('d', 'c')
+    # custom_graph.remove_edge('d', 'a')
+
+    # print(custom_graph)
+    adjacency_list = {
+        'A': ['B', 'C', 'D'],
+        'B': ['A', 'C'],
+        'C': ['A', 'B', 'D'],
+        'D': ['A', 'C']
+    }
+    # custom_graph.remove_vertex('d')
+    # print('After deleting vertex D')
     print(custom_graph)
-    custom_graph.remove_edge('a', 'd')
-    # custom_graph.remove_edge('b', 'c')
-    # custom_graph.remove_edge('c', 'a')
-    print(custom_graph)
+    custom_graph.breath_first_search('a')
+    # delete vertex of D
